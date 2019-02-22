c413ab3e5ea3943aa52b9e17571e798f96ce7d98
Chang Wang <garnwraly@gmail.com>
2019-02-22 13:46:09 -0500

    Use mongodb-memory-server to remove dependency on full mongo instance

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import { ApolloServer, PubSub } from "apollo-server";
 import mongoose from "mongoose";
+import MongoMemoryServer from 'mongodb-memory-server';
+
 import { GraphQLSchema } from "graphql";
 import { mergeSchemas } from "graphql-tools";
+
 const { OAuth2Client } = require("google-auth-library");
+const mongoServer = new MongoMemoryServer();
 
 import dotenv from "dotenv";
 import {
 	CLIENT_ID,
-	DB_NAME,
-	MONGO_PORT,
-	MONGO_URL
 } from "./common/util/secrets";
 import { userController } from "./user/user.controller";
 import schemas from "./schema";
@@ -24,31 +25,57 @@ export const client = new OAuth2Client(CLIENT_ID);
 // help to debug mongoose
 mongoose.set("debug", true);
 
-mongoose.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/${DB_NAME}`);
+mongoose.Promise = Promise;
 
-const schema: GraphQLSchema = mergeSchemas({
-	schemas,
-	resolvers
-});
+mongoServer.getConnectionString().then((mongoUri) => {
+  const mongooseOpts = {
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+  };
+ 
+  mongoose.connect(mongoUri, mongooseOpts);
+ 
+  mongoose.connection.on('error', (e) => {
+    if (e.message.code === 'ETIMEDOUT') {
+      console.log(e);
+      mongoose.connect(mongoUri, mongooseOpts);
+    }
+    console.log(e);
+  });
+ 
+  mongoose.connection.once('open', () => {
+    console.log(`MongoDB successfully connected to ${mongoUri}`);
+	});
+	
+	// mongoose.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/${DB_NAME}`);
 
-// GraphQL
-const server = new ApolloServer({
-	schema,
-	context: async ({ req }: any) => {
-		if (!req || !req.headers) {
-			return;
-		}
+	const schema: GraphQLSchema = mergeSchemas({
+		schemas,
+		resolvers
+	});
+	
+	// GraphQL
+	const server = new ApolloServer({
+		schema,
+		context: async ({ req }: any) => {
+			if (!req || !req.headers) {
+				return;
+			}
+	
+			const token = req.headers.authorization || "";
+			const checkToken = await userController.findOrCreateUser(token);
+			if (!checkToken.hasOwnProperty("authorized")) {
+				return { user: checkToken, authorized: true };
+			}
+			return checkToken;
+		},
+		tracing: true
+	});
+	
+	server.listen().then(({ url }) => {
+		console.log(`🚀 Server ready at ${url}`);
+	});
+	
+})
 
-		const token = req.headers.authorization || "";
-		const checkToken = await userController.findOrCreateUser(token);
-		if (!checkToken.hasOwnProperty("authorized")) {
-			return { user: checkToken, authorized: true };
-		}
-		return checkToken;
-	},
-	tracing: true
-});
-
-server.listen().then(({ url }) => {
-	console.log(`🚀 Server ready at ${url}`);
-});
